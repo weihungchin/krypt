4be7f5ffac831af1dec369e42c2a814955caab9d
WeiHung <wei_hung.chin@daimler.com>
2022-05-08 17:44:43 +0800

    misc fixes

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -35,7 +35,7 @@ const Welcome = () => {
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start items-start flex-col mf:mr-80">
+                <div className="flex flex-1 justify-start items-start flex-col mf:mr-8">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
                         Send Crypto <br /> across the world
                     </h1>
